baf351df2de5abbe86d870a2418357bb43f92a80
hana-bananaa <debartosiaki@gmail.com>
2021-07-08 11:17:59 -0300

    aprendendo sobre ternarios

---
+++ javascript2/exemplos/arrow-function.js
@@ -0,0 +1,12 @@
+// function
+ function soma (a,b){
+     return a + b
+ }
+ console.log(soma(3,5))
+
+ // arrow function
+
+// const soma = (num1, num2) => num1+ num2
+
+ const sayHello = name => `Hello ${name}`
+ console.log(sayHello('MARI'))
