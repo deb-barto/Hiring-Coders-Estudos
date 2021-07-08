11c4add426a7c6f924b1ba9e2a2fed834ad08b50
hana-bananaa <debartosiaki@gmail.com>
2021-07-08 10:57:34 -0300

    aprendendo sobre ternarios

---
+++ javascript2/exemplos/template-string.js
@@ -0,0 +1,5 @@
+"debora"
+let role = "transformadora"
+
+console.log(` Simara é ${role}`)
+//  logo acima aprendemos como se faz uma interpolação
---
+++ javascript2/exemplos/ternarios.js
@@ -0,0 +1,5 @@
+function calcula(idade) // if e else // a primeira é true a segunda é false
+    {
+        return idade >=18 ? 'Maior de idade' : 'Menor de idade'
+    }
+    console.log(calcula(15))
